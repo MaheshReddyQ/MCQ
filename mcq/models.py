e859e999e2821fc17545f183dc9c8fb592eda70a
MaheshReddyQ <136079458+MaheshReddyQ@users.noreply.github.com>
2023-07-20 11:43:26 +0530

    Delete mcq directory

--- mcq/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
-    choice_text = models.CharField(max_length=200)
-    is_correct = models.BooleanField(default=False)
